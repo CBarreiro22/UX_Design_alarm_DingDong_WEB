592f0a17fdafa8974324b5e706d130f7b49af23f
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2023-09-28 08:47:01 -0500

    Add action page

--- ding-dong-angular/src/app/actions/actions.component.ts
+++ ding-dong-angular/src/app/actions/actions.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Sort } from '@angular/material/sort';
 
+
+export interface  Action {
+  description: string;
+}
 @Component({
   selector: 'app-actions',
   templateUrl: './actions.component.html',
   styleUrls: ['./actions.component.css']
 })
 export class ActionsComponent {
+  selectedValue: string;
+  actions: Action[] = [
+    { description: 'Reproducir Musica'},
+    { description: 'Realizar llamada'},
+    { description: 'Enviar Mensaje'},
+  ];
 
+  sortedData: Action[];
+  constructor() {
+    this.sortedData = this.actions.slice();
+    this.selectedValue = '';
+  }
+  sortData(sort: Sort) {
+    const data = this.actions.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+  }
 }
